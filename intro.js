f9e124192c6037d844bc73b1cd33f914fa0c0027
Mubarak710 <mubaraktech710@gmail.com>
2026-04-10 07:51:01 +0000

    Add intro.js with personal information logging

---
+++ intro.js
@@ -0,0 +1,8 @@
+
+let name = "Mubarak";
+let age = 20;
+let favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
